f5609b4a680ac7f0374d7d4a147d5b26365ff479
Abhay2546 <abhayzingreb@gmail.com>
2026-07-06 01:10:09 +0530

    testing

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -231,7 +231,7 @@ const Index = () => {
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.8, delay: 1 }}
             >
-              testing.
+              Hello Abhay.
             </motion.p>
             
             <motion.div
